b845c0872d8febb090242e30e0f7f870690ff568
stvy886 <stevanuschang946@gmail.com>
2026-05-07 20:20:05 +0700

    Update Slave_manual_3_boxgriper.ino

--- Robot-Manual/Slave_manual_3_boxgriper/Slave_manual_3_boxgriper.ino
+++ Robot-Manual/Slave_manual_3_boxgriper/Slave_manual_3_boxgriper.ino
@@ -1,91 +1,120 @@
 #include <ModbusRtu.h>
 #include <Servo.h>
 
-#define addr 3
-#define TXEN 4
-bool srvoState = false;
-bool lastPos = false;
-
-Modbus slave(addr, Serial, TXEN);
-
-uint16_t au16data[2] = {0, 0};
-
 Servo srv1;
 Servo srv2;
 
-const int pwmMotor1 = 5;
-const int pwmMotor2 = 6;  
+const int pinSensor = 2;
+volatile unsigned long pulses = 0;
 
-const int pinMotor1 = 2;
-const int pinMotor2 = 3;
+// Pins
+#define rpwm 5
+#define lpwm 6
+#define pwm1 9
+#define pwm2 10
+#define en 4 
 
-int data = 0;
-int motorData = 0;
+int pos = 0;
+int max_first = 60;
+int min_first = 58;
+int max_target = 120;
+int min_target = 118;
+
+bool lastPos1 = false;
+bool lastPos2 = false;
+
+bool servoState1 = false; 
+bool servoState2 = false; 
+
+// Data Modbus
+uint16_t au16data[1] = {0};
+
+// Inisiasi modbus slave
+Modbus slave(5, Serial, en);
+
+void pulseCounter(){
+  pulses++;
+}
+
+void slider(int pulse, int max, int min){
+  if (pulse > max){
+    analogWrite(rpwm, 50);
+    analogWrite(lpwm, 0);
+  } else if (pulse < min){
+    analogWrite(rpwm, 0);
+    analogWrite(lpwm, 50);
+  } else {
+    analogWrite(rpwm, 0);
+    analogWrite(lpwm, 0);
+  }
+}
+
+void updateGriper(){
+  pos = (int16_t)au16data[0];
+
+  if (pos == 1){
+    if (!lastPos1){
+      servoState1 = !servoState1;
+
+      if (servoState1){
+        srv1.write(120);
+      } else {
+        srv1.write(0);
+      }
+    }
+    lastPos1 = true;
+  } else {
+    lastPos1 = false;
+  }
+
+  if (pos == 2){
+    if (!lastPos2){
+      servoState2 = !servoState2;
+
+      if (servoState2){
+        slider(pulses, max_target, min_target);
+        srv2.write(0);
+      } else {
+        srv2.write(90);
+        slider(pulses, max_first, min_first);
+      }
+    }
+    lastPos2 = true;
+  } else {
+    lastPos2 = false;
+  }
+}
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  pinMode(rpwm, OUTPUT);
+  pinMode(lpwm, OUTPUT);
+
+  analogWrite(rpwm, 0);
+  analogWrite(lpwm, 0);
+
+  pinMode(pinSensor, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinSensor), pulseCounter, FALLING);
+
+  srv1.attach(pwm1);
+  srv2.attach(pwm2);
+
+  srv1.write(0);
+  srv2.write(90);
+
+  pinMode(en, OUTPUT);
   slave.start();
 
-  srv1.attach(10);
-  srv2.attach(9);
-
-  srv1.write(180);
-  srv2.write(0);
-
-  pinMode(pinMotor1, OUTPUT);
-  pinMode(pinMotor2, OUTPUT);
-  pinMode(pwmMotor1, OUTPUT);
-  pinMode(pwmMotor2, OUTPUT);
-
-  digitalWrite(pinMotor1, LOW);
-  digitalWrite(pinMotor2, LOW);
-  analogWrite(pwmMotor1, 0);
-  analogWrite(pwmMotor2, 0);
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  slave.poll(au16data, 2);
+  slave.poll(au16data, 1);
 
-  data = (int16_t)au16data[0];
-  motorData = (int16_t)au16data[1];
+  interrupts();
+  updateGriper();
 
-  // GRIPPER
-  if (data == 1) {
-    if (!lastPos) {
-      lastPos = true;
-      srvoState = !srvoState; 
-      if (srvoState) {
-        srv1.write(100);
-        srv2.write(60);
-      } else {
-        srv1.write(180);
-        srv2.write(0);
-      }
-    } 
-  } else {
-    lastPos = false;
-  }
-  
-  //MOTOR
-  if (motorData == 3) {
-    digitalWrite(pinMotor1, HIGH);
-    digitalWrite(pinMotor2, LOW);
-    analogWrite(pwmMotor1, 175);
-    analogWrite(pwmMotor2, 0);
-  }
-  else if (motorData == 2) {
-    digitalWrite(pinMotor1, LOW);
-    digitalWrite(pinMotor2, HIGH);
-    analogWrite(pwmMotor1, 0);
-    analogWrite(pwmMotor2, 175);
-  }
-  else {
-    digitalWrite(pinMotor1, LOW);
-    digitalWrite(pinMotor2, LOW);
-    analogWrite(pwmMotor1, 0);
-    analogWrite(pwmMotor2, 0);
-  }
 }
